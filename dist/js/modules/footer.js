63c80203e7cac22a27aaa690f1cf9fb321ee4e23
Chare023 <branislavcharkic@gmail.com>
2022-12-20 00:30:40 +0100

    Created JSON db, refactored footer to dynamically print data to html

--- dist/js/modules/footer.js
+++ dist/js/modules/footer.js
@@ -1,83 +1,74 @@
+import Data from './functions.js';
+
+let arrow = `<i class="fa fa fa-chevron-right link-arrow"></i>`;
+let ftWrap = '';
+
+Data.footer.secwrap.forEach(element => {
+    ftWrap += `
+    <div class="ft-wrap">
+        <div class="ft-wrap-sec">`
+
+    if (element.title[1].length <= 1) {
+        ftWrap += `<h3>${element.title}</h3>`
+    } else {
+        Object.values(element.title).forEach(title => {
+            ftWrap += `<h3>${title}</h3>`
+        })
+    }
+
+    if (element.content) {
+        if (element.content[1].length <= 1) {
+            ftWrap += `<p>${element.content}</p>`
+        } else {
+            Object.values(element.content).forEach(content => {
+                ftWrap += `<p>${content}</p>`
+            })
+        }
+    }
+
+    if (element.link[1].length <= 1) {
+        ftWrap += `<a href="" class="ft-link">${arrow} ${element.link}</a>`
+    } else {
+        Object.values(element.link).forEach(el => {
+            ftWrap += `<a href="" class="ft-link">${arrow} ${el}</a>`
+        })
+    }
+
+    ftWrap += `</div>
+        </div>`
+})
+
 let ft = `
-            <div class="ft-container">
-            <div class="ft-wrap">
-                <img src="img/logo.png" alt="" class="ft-logo">
-            </div>
-
-            <div class="ft-wrap">
-                <div class="ft-wrap-sec">
-                    <h3>About nazivsajta</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis impedit nisi possimus dolore
-                        accusamus
-                        adipisci. Porro rem saepe voluptatem deleniti provident ipsam debitis adipisci eaque voluptate,
-                        quas,
-                        sint, numquam fugit?</p>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> The Privacy Policy for
-                        "Naziv
-                        Sajta"</a>
-                </div>
-            </div>
-
-            <div class="ft-wrap">
-                <div class="ft-wrap-sec">
-                    <h3>Tourist Attractions</h3>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> City Center</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Petrovaradin</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Novi Sad on the
-                        Danube</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Fortress</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Fruska Gora</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Wineries</a>
-                </div>
-            </div>
-
-            <div class="ft-wrap">
-                <div class="ft-wrap-sec">
-                    <h3>Frequently visited pages</h3>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Things to Do</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Destinations</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Nature & Climate</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> When to go</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Culture</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Frequently Asked
-                        Questions</a>
-                </div>
-            </div>
-
-            <div class="ft-wrap">
-                <div class="ft-wrap-sec">
-                    <h3>Info for tourists</h3>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Tourist Information
-                        centers</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Service Information</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> How to reach us</a>
-                    <a href="" class="ft-link"><i class="fa fa fa-chevron-right link-arrow"></i> Public transport</a>
-                </div>
-            </div>
-
-            <div class="ft-wrap">
-                <div class="ft-wrap-sec">
-                    <h3>Contact Us</h3>
-                    <p><i class="fa fa-map-marker"></i> Lorem ipsum</p>
-                    <p><i class="fa fa-phone"></i> (+381) 23 45 678</p>
-                    <p><i class="fa fa-envelope"></i> info@nazivsajta.com</p>
-                    <p><i class="fa fa-clock-o"></i> Mon. - Fri. 8am - 4pm</p>
-                </div>
-            </div>
+    <div class="ft-container">
+        <div class="ft-wrap">
+           <img src="img/logo.png" alt="" class="ft-logo">
         </div>
 
-        <div class="ft-social-icons">
-            <a class="insta-container" title="Instagram" href=""><i class="fa fa-instagram"></i></a>
-            <a class="fb-container" title="Facebook" href=""><i class="fa fa-facebook"></i></a>
-            <a class="twitter-container" title="Twitter" href=""><i class="fa fa-twitter"></i></a>
-            <a class="yt-container" title="Youtube" href=""><i class="fa fa-youtube-play"></i></a>
-            <a class="in-container" title="Linkedin" href=""><i class="fa fa-linkedin"></i></i></a>
-        </div>
+        ${ftWrap}
 
-        <div class="copyright">
-            <i class="fa fa-copyright"></i>
-            <p> Copyright 2023 The Stranger Scripts</p>
+        <div class="ft-wrap">
+            <div class="ft-wrap-sec">
+                <h3>Contact Us</h3>
+                <p><i class="fa fa-map-marker"></i> Lorem ipsum</p>
+                <p><i class="fa fa-phone"></i> (+381) 23 45 678</p>
+                <p><i class="fa fa-envelope"></i> info@nazivsajta.com</p>
+                <p><i class="fa fa-clock-o"></i> Mon. - Fri. 8am - 4pm</p>
+            </div>
         </div>
-`;
+    </div>
+
+    <div class="ft-social-icons">
+        <a class="insta-container" title="Instagram" href=""><i class="fa fa-instagram"></i></a>
+        <a class="fb-container" title="Facebook" href=""><i class="fa fa-facebook"></i></a>
+        <a class="twitter-container" title="Twitter" href=""><i class="fa fa-twitter"></i></a>
+        <a class="yt-container" title="Youtube" href=""><i class="fa fa-youtube-play"></i></a>
+        <a class="in-container" title="Linkedin" href=""><i class="fa fa-linkedin"></i></i></a>
+    </div>
+
+    <div class="copyright">
+        <i class="fa fa-copyright"></i>
+        <p> Copyright 2023 The Stranger Scripts</p>
+    </div>
+`
 
 export default ft;
