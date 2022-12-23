fbf0881650893c0278faa52a5d57dbebbdebceaa
Slou A <slowautomobile@protonmail.com>
2022-12-23 17:12:41 +0100

    Nav scrollable background func moved to header.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 // Import modules
-import Nav from './modules/header.js';
+import * as header from './modules/header.js';
 import Ft from './modules/footer.js';
 
 // Get Html elements
@@ -7,38 +7,10 @@ let nav = document.getElementById('nav-container');
 let ft = document.getElementById('ft-container');
 
 // Inner content to html elements
-nav.innerHTML = Nav;
+nav.innerHTML = header.nav;
 ft.innerHTML = Ft;
 
 // Ovde se mogu dodati neke opste funkcionalnosti sajta
 
-// Promena Navbar backgroud-a na scroll
-let prevScrollpos = window.pageYOffset;
-let navbarElement = document.querySelector('.navbar');
-
-if (window.matchMedia('(max-width: 960px)').matches) {
-  navbarElement.classList.add('bg-nav-white-transparent');
-} else {
-  navbarElement.classList.add('bg-nav-white-gradient');
-}
-
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos && window.pageYOffset < 114) {
-    if (window.matchMedia('(max-width: 960px)').matches) {
-      navbarElement.classList.remove('bg-nav-white-gradient');
-      navbarElement.classList.add('bg-nav-white-transparent');
-    } else {
-      navbarElement.classList.add('bg-nav-white-gradient');
-      navbarElement.classList.remove('bg-nav-white-transparent');
-      navbarElement.classList.add('pb-lg-5');
-      navbarElement.classList.remove('pb-lg-1');
-    }
-  } else {
-    navbarElement.classList.remove('bg-nav-white-gradient');
-    navbarElement.classList.remove('pb-lg-5');
-    navbarElement.classList.add('bg-nav-white-transparent');
-    navbarElement.classList.add('pb-lg-1');
-  }
-  prevScrollpos = currentScrollPos;
-};
+// Promena Navbar background-a na scroll
+header.navbarBackgroundScroll();
