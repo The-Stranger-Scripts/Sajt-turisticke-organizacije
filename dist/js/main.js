ec163dd41e01d0686589b6109081722df53445a9
Slou A <slowautomobile@protonmail.com>
2022-12-26 17:20:15 +0100

    change language dynamic example

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,39 @@
 // Import modules
 import * as header from './modules/header.js';
-import Ft from './modules/footer.js';
+import writeFooter from './modules/footer.js';
 
 // Get Html elements
 let nav = document.getElementById('nav-container');
 let ft = document.getElementById('ft-container');
 
 // Inner content to html elements
+let db = await getData();
+
 nav.innerHTML = header.nav;
-ft.innerHTML = Ft;
+ft.innerHTML = writeFooter(db.en_EN);
+
+// Prebačeno iz functions.js
+async function getData() {
+  return fetch('db/db.json').then(response => {
+    return response.json();
+  });
+}
+
 
 // Ovde se mogu dodati neke opste funkcionalnosti sajta
+const srOption = document.getElementById('sr');
+const enOption = document.getElementById('en');
+
+srOption.addEventListener('click', () => {
+  ft.innerHTML = writeFooter(db.sr_SR);
+  // ovde mogu biti dodate slične funkcije za ispis headera i main-a 
+});
+enOption.addEventListener('click', () => {
+  ft.innerHTML = writeFooter(db.en_EN);
+});
+
+
 
 // Promena Navbar background-a na scroll
 header.navbarBackgroundScroll();
+
