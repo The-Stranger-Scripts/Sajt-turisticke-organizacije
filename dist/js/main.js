644592a72c79d269109eb31b7aad5120b5123633
Slou A <slowautomobile@protonmail.com>
2022-12-30 15:29:40 +0100

    dynamic language testing

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,45 +9,85 @@ let ft = document.getElementById('ft-container');
 // Set initial language in localStorage
 let lang = localStorage.getItem('language');
 if (!lang) {
-  lang = localStorage.setItem('language', 'sr_SR');
+  localStorage.setItem('language', 'en_EN');
+  lang = localStorage.getItem('language');
 }
 
+let srOption;
+let x;
 
-nav.innerHTML = header.nav;
+function getty() {
+  fetch('db/db.json')
+    .then(res => res.json())
+    .then(data => {
+      lang = localStorage.getItem('language');
+      nav.innerHTML = header.writeHeader(data[`${lang}`]);
+      header.navbarBackgroundScroll();
+      ft.innerHTML = writeFooter(data[`${lang}`]);
+      
+    });
+    srOption = document.getElementById('sr');
+    x = 33;
+}
+
+getty();
+console.log(srOption, x);
+
+// await initGetData();
+
+// nav.innerHTML = header.writeFooter(lang);
 // ft.innerHTML = writeFooter(data);
 
 // Prebačeno iz functions.js
-async function getData(lang) {
-  return fetch('db/db.json')
-    .then(res => res.json())
-    .then(data => {
-      lang = localStorage.getItem('language')
-      console.log(lang);
-      ft.innerHTML = writeFooter(data[`${lang}`])
-      // ovde mogu biti dodate slične funkcije za ispis headera i main-a
-    })
-};
+// async function getData(lang) {
+//   return fetch('db/db.json')
+//     .then(res => res.json())
+//     .then(data => {
+//       lang = localStorage.getItem('language');
+
+//       // header.writeHeaderTitles(data[`${lang}`])
+//       // nav.innerHTML = header.writeHeader(data[`${lang}`]);
+//       ft.innerHTML = writeFooter(data[`${lang}`]);
+
+//       // header.navbarBackgroundScroll();
+//       // getButtons();
+
+//       // ovde mogu biti dodate slične funkcije za ispis headera i main-a
+//     })
+// };
 
 // Inner content to html elements
-await getData(lang);
+// await getData(lang);
+
+// Promena Navbar background-a na scroll
+// header.navbarBackgroundScroll();
 
 // Get btns
 // Ovo ce morati da se sredi kada se napravi dinamicki
 // ispis navigacije, jer nece moci da nadje btn elemente
 // pre nego sto ih kreira.
 // ->
-const srOption = document.getElementById('sr');
-const enOption = document.getElementById('en');
 
-srOption.addEventListener('click', () => {
-  lang = localStorage.setItem('language', 'sr_SR');
-  getData(lang);
-});
-enOption.addEventListener('click', () => {
-  lang = localStorage.setItem('language', 'en_EN');
-  getData(lang);
-});
+// document.getElementById('sr').addEventListener('click', () => {
+//   localStorage.setItem('language', 'sr_SR');
+//   // getData();
+//   console.log('sss')
+// });
+// document.getElementById('en').addEventListener('click', () => {
+//   localStorage.setItem('language', 'en_EN');
+//   // getData();
+
+//   console.log('adsd')
+// });
+
+// srOption.addEventListener('click', () => {
+//   lang = localStorage.setItem('language', 'se_SR');
+//   getData(lang);
+//   console.log('asdewq')
+// })
+// enOption.addEventListener('click', () => {
+//   lang = localStorage.setItem('language', 'en_EN');
+//   getData(lang);
+//   console.log('1234332')
+// })
 // <- ovaj block
-
-// Promena Navbar background-a na scroll
-header.navbarBackgroundScroll();
--- dist/js/modules/home.js
+++ dist/js/modules/home.js
@@ -1,1 +1,17 @@
 // Ovde se mogu kuckati funkcionalnosti vezane za Home stranicu!
+// let buttons = {};
+
+// buttons = document.addEventListener('DOMContentLoaded', () =>{
+//   return { 
+//     srOption: document.getElementById('sr'),
+//     enOption: document.getElementById('en')
+//   }
+// })
+// let srOption;
+// async function getButtons() {
+//   await srOption = document.getElementById('sr');
+// }
+// getButtons();
+
+
+// console.log(srOption);
