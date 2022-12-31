01b283d640d4e04f9962e4474967d56233d3b49a
Chare023 <branislavcharkic@gmail.com>
2022-12-31 11:39:03 +0100

    Fixed main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,35 +20,48 @@ async function initData() {
       lang = localStorage.getItem('language')
       nav.innerHTML = header.writeHeader(data[`${lang}`]);
       ft.innerHTML = writeFooter(data[`${lang}`]);
+    }).then(() => {
+      const srOption = document.getElementById('sr');
+      const enOption = document.getElementById('en');
+
+      srOption.addEventListener('click', () => {
+        lang = localStorage.setItem('language', 'sr_SR');
+        initData();
+      });
+      enOption.addEventListener('click', () => {
+        lang = localStorage.setItem('language', 'en_EN');
+        initData();
+      });
+      header.navbarBackgroundScroll();
     })
 };
 
 // Inner content to html elements
 await initData();
 
-// On-click loading and rendering header, footer, etc.
-async function getData() {
-  return fetch('db/db.json')
-    .then(res => res.json())
-    .then(data => {
-      lang = localStorage.getItem('language')
-      header.writeHeaderTitles(data[`${lang}`])
-      ft.innerHTML = writeFooter(data[`${lang}`]);
-      // ovde mogu biti dodate slične funkcije za ispis headera i main-a
-    })
-};
+// // On-click loading and rendering header, footer, etc.
+// async function getData() {
+//   return fetch('db/db.json')
+//     .then(res => res.json())
+//     .then(data => {
+//       lang = localStorage.getItem('language')
+//       header.writeHeaderTitles(data[`${lang}`])
+//       ft.innerHTML = writeFooter(data[`${lang}`]);
+//       // ovde mogu biti dodate slične funkcije za ispis headera i main-a
+//     })
+// };
 
-const srOption = document.getElementById('sr');
-const enOption = document.getElementById('en');
+// const srOption = document.getElementById('sr');
+// const enOption = document.getElementById('en');
 
-srOption.addEventListener('click', () => {
-  lang = localStorage.setItem('language', 'sr_SR');
-  getData();
-});
-enOption.addEventListener('click', () => {
-  lang = localStorage.setItem('language', 'en_EN');
-  getData();
-});
+// srOption.addEventListener('click', () => {
+//   lang = localStorage.setItem('language', 'sr_SR');
+//   initData();
+// });
+// enOption.addEventListener('click', () => {
+//   lang = localStorage.setItem('language', 'en_EN');
+//   initData();
+// });
 
 // Change Navbar background on scroll
-header.navbarBackgroundScroll();
+// header.navbarBackgroundScroll();
